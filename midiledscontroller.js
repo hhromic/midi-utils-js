d7cfa59981b03e4edf2ac1f3dfdd570562f40cbb
Hugo Hromic <hhromic@gmail.com>
2014-03-20 00:25:08 +0000

    MidiLedsController is an Event Emitter. Better handling of display frames.

--- midiledscontroller.js
+++ midiledscontroller.js
@@ -9,78 +9,78 @@
     'use strict';
 
     // Constructor
-    function MidiLedsController(numLeds, noteOnColors, noteOffColors) {
-        this._numLeds = numLeds;
-        this._noteOnColors = noteOnColors;
-        this._noteOffColors = noteOffColors;
-        this._noteOffset = 0x00;
-        this._offVelocity = 0x00;
+    function MidiLedsController(noteColors, noteMin, noteMax, ledsData) {
+        EventEmitter.call(this);
 
-        // Setup event handlers
-        this._handlers = {
-            'led-output': undefined,  // led, r, g, b
-        };
+        // Instance variables
+        this._noteColors = noteColors;
+        this._noteMin = noteMin;
+        this._noteMax = noteMax;
+        this._ledsData = ledsData;
+
+        // Create internal state arrays
+        this._leds = {start: [], decay: []}
+        for (var i=0; i<(noteMax - noteMin + 1); i++) {
+            this._leds.start.push(-1);
+            this._leds.decay.push(undefined);
+        }
     }
+
+    // We are an event emitter
+    MidiLedsController.prototype = Object.create(EventEmitter.prototype);
 
     // Cache variable for prototype
     var proto = MidiLedsController.prototype;
 
-    // Assign an event handler to a LED strip event
-    proto.on = function (event, handler) {
-        if (typeof handler === 'function' && this._handlers.hasOwnProperty(event)) {
-            this._handlers[event] = handler;
-            return true;
-        }
-        return false;
-    }
+    // Compute next leds frame
+    proto._computeNextFrame = function (time) {
+        var redraw = false;
+        for (var led=0; led<(this._noteMax - this._noteMin + 1); led++) {
+            if (this._leds.start[led] == 0)
+                this._leds.start[led] = time;
+            if (this._leds.start[led] > 0) {
+                var elapsed = time - this._leds.start[led];
+                var factor = (1 - (elapsed / 15000)) * (this._leds.decay[led] == 'slowDecay' ? 1 : 0.65);
+                if (factor >= 0) {
+                    var r = Math.floor(((this._ledsData[led] >> 16) & 0xFF) * factor);
+                    var g = Math.floor(((this._ledsData[led] >> 8) & 0xFF) * factor);
+                    var b = Math.floor((this._ledsData[led] & 0xFF) * factor);
 
-    // Emit LED strip events
-    proto.emit = function (event, data1, data2, data3, data4) {
-        if (this._handlers.hasOwnProperty(event) && this._handlers[event] !== undefined) {
-            switch (event) {
-                case 'led-output':
-                    this._handlers[event](data1, data2, data3, data4);
-                    break;
-                default:
-                    return false;
+                    var newValue = (r << 16) + (g << 8) + b;
+                    if (newValue != this._ledsData[led]) {
+                        redraw = true;
+                        this._ledsData[led] = newValue;
+                    }
+                }
+                else this._leds.start[led] = -1;
             }
-            return true;
         }
-        return false;
-    }
-
-    // Set the offset to be substracted to every note
-    proto.setNoteOffset = function (note) {
-        if (note < 0x00 || note > 0x7F)
-            return false;
-        this._noteOffset = note;
-        return true;
-    }
-
-    // Set the velocity value for 'note-off'
-    proto.setOffVelocity = function (velocity) {
-        if (velocity < 0x00 || velocity > 0x7F)
-            return false;
-        this._offVelocity = velocity;
-        return true;
+        return redraw;
     }
 
     // Process a Midi Note On message
     proto.noteOn = function (channel, note, velocity) {
-        if (channel < 0x0 || channel > 0xF || note < 0x00 || note > 0x7F || velocity < 0x00 || velocity > 0x7F)
+        if (channel < 0x0 || channel > 0xF || note < this._noteMin || note > this._noteMax || velocity < 0x00 || velocity > 0x7F)
             return false;
-        var color = this._noteOnColors.mapNoteRGB(note, velocity);
-        this.emit('led-output', note - this._noteOffset, color.r, color.g, color.b);
+        var color = this._noteColors.mapNoteRGB(note, velocity);
+        this._ledsData[note - this._noteMin] = (color.r << 16) + (color.g << 8) + color.b;
+        this._leds.start[note - this._noteMin] = 0;
+        this._leds.decay[note - this._noteMin] = 'slowDecay';
         return true;
     }
 
     // Process a Midi Note Off message
     proto.noteOff = function (channel, note) {
-        if (channel < 0x0 || channel > 0xF || note < 0x00 || note > 0x7F)
+        if (channel < 0x0 || channel > 0xF || note < this._noteMin || note > this._noteMax)
             return false;
-        var color = this._noteOffColors.mapNoteRGB(note, this._offVelocity);
-        this.emit('led-output', note - this._noteOffset, color.r, color.g, color.b);
+        this._leds.decay[note - this._noteMin] = 'fastDecay';
         return true;
+    }
+
+    // Signal a clock ticket for the leds controller
+    proto.tick = function(time) {
+        if (this._computeNextFrame(time))
+            this.emit('frame');
     }
 
     // Expose the class either via AMD, CommonJS or the global object
