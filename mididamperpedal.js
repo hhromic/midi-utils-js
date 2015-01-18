f0b4a8a563036caaf96e06f264d843951e89ff82
Hugo Hromic <hhromic@gmail.com>
2015-01-18 12:27:45 +0000

    Improved documentation

--- mididamperpedal.js
+++ mididamperpedal.js
@@ -1,14 +1,22 @@
 /**
- * MidiDamperPedal v1.0 - A very simple event-driven Midi Damper Pedal processor for JavaScript.
- * Hugo Hromic - http://github.com/hhromic
+ * MidiDamperPedal v1.0 - https://github.com/hhromic/midi-utils-js
+ * A very simple event-driven MIDI damper pedal emulator for JavaScript.
  * MIT license
+ * Hugo Hromic - http://github.com/hhromic
+ *
+ * @requires EventEmitter
  */
-/*jslint nomen: true*/
+/*jslint nomen: true */
 
-(function () {
+;(function () {
     'use strict';
 
-    // Constructor
+    /**
+     * Represents a MIDI damper pedal emulator.
+     *
+     * @public
+     * @constructor
+     */
     function MidiDamperPedal() {
         EventEmitter.call(this);
         this._pressed = 0x0000;
@@ -20,18 +28,28 @@
     // We are an event emitter
     MidiDamperPedal.prototype = Object.create(EventEmitter.prototype);
 
-    // Prototype shortcut
+    // Shortcuts to improve speed and size
     var proto = MidiDamperPedal.prototype;
 
-    // Simulate pressing the damper pedal
+    /**
+     * Simulates pressing the damper pedal.
+     *
+     * @public
+     * @param {number} channel - the MIDI channel.
+     */
     proto.press = function (channel) {
         this._pressed |= 1 << (channel & 0xF);
     }
 
-    // Simulate releasing the damper pedal
+    /**
+     * Simulates releasing (depressing) the damper pedal.
+     *
+     * @public
+     * @param {number} channel - the MIDI channel.
+     */
     proto.release = function (channel) {
         this._pressed &= ~(1 << (channel & 0xF));
-        for (var i=128; i--;) { // Send Note Off messages for all channel held notes
+        for (var i=128; i--;) { // Send Note-Off messages for all held notes of the channel
             if ((this._heldNotes[channel & 0xF][i / 32] >> (i % 32)) & 1) {
                 this._heldNotes[channel & 0xF][i / 32] &= ~(1 << (i % 32));
                 this.emit('note-off', channel, i, 0x00);
@@ -39,16 +57,30 @@
         }
     }
 
-    // Process a Midi Note On message
+    /**
+     * Processes and forwards a MIDI Note-On message.
+     *
+     * @public
+     * @param {number} channel - the MIDI channel.
+     * @param {number} note - the MIDI note.
+     * @param {number} velocity - the velocity of the MIDI note.
+     */
     proto.noteOn = function (channel, note, velocity) {
         if ((this._heldNotes[channel & 0xF][(note & 0x7F) / 32] >> ((note & 0x7F) % 32)) & 1)
             this._heldNotes[channel & 0xF][(note & 0x7F) / 32] &= ~(1 << ((note & 0x7F) % 32)); // Reset channel held note
         this.emit('note-on', channel, note, velocity);
     }
 
-    // Process a Midi Note Off message
+    /**
+     * Processes and forwards (if necessary) a MIDI Note-Off message.
+     *
+     * @public
+     * @param {number} channel - the MIDI channel.
+     * @param {number} note - the MIDI note.
+     * @param {number} velocity - the velocity of the MIDI note.
+     */
     proto.noteOff = function (channel, note, velocity) {
-        if ((this._pressed >> (channel & 0xF)) & 1) // If pedal pressed, hold channel Note Off message
+        if ((this._pressed >> (channel & 0xF)) & 1) // If pedal pressed, hold channel Note-Off message
             this._heldNotes[channel & 0xF][(note & 0x7F) / 32] |= 1 << ((note & 0x7F) % 32);
         else
             this.emit('note-off', channel, note, velocity);
