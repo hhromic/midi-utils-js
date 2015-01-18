b358865c7b0b67bcf07c5062c285cc2223970563
Hugo Hromic <hhromic@gmail.com>
2015-01-18 13:21:00 +0000

    Added gitignore and meta script for bundling

---
+++ midiutils.js
@@ -0,0 +1,9 @@
+/**
+ * MIDI utils v1.0 - https://github.com/hhromic/midi-utils-js
+ * A very simple collection of utilities to process MIDI data for JavaScript.
+ * MIT license
+ * Hugo Hromic - http://github.com/hhromic
+ *
+ * @requires EventEmitter
+ * @preserve
+ */
